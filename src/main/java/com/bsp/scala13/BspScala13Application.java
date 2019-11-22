6921b07e3d5b8d2baaa95b45c0fc7f0e2e8436a6
Administrator <1713716789@qq.com>
2019-11-23 01:04:23 +0800

    Initial commit

---
+++ src/main/java/com/bsp/scala13/BspScala13Application.java
@@ -0,0 +1,13 @@
+package com.bsp.scala13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspScala13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BspScala13Application.class, args);
+    }
+
+}
